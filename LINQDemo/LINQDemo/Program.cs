09c29ae5e95a316f38970ae28533a5a7d65d9850
Bipeen <acharyab@seas.upenn.edu>
2018-11-28 12:35:29 -0800

    Files and LINQs

---
+++ LINQDemo/LINQDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 0, 1, 2, 3, 4, 5, 6 };
+
+            var evenNumQuery =
+                from num in numbers
+                where (num % 2) == 0
+                select num;
+
+            foreach (int i in evenNumQuery) {
+                Console.WriteLine("{0} is an even number", i);
+            }
+        }
+    }
+}
